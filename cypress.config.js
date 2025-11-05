0c8d3c44ea0f47c75557af9be9882129180bf4fb
Benjamin427 <shadowboxer79@hotmail.com>
2025-11-05 16:17:14 -0500

    Changes were made in this file

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 const builder = require('@bahmutov/cypress-esbuild-preprocessor')
 const addCucumberpreprocessor = require('cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin
-const createEsbuilder = require('cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin
+const createEsbuilder = require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
